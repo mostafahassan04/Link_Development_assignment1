4688ca39f6da77ce1250475fed4ab2a7b1ce053e
Mostafa Hassan <moustafahassan028@gmail.com>
2026-07-20 23:34:49 +0300

    fix: fix profile route name

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,15 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
-import HomeIcon from "../../assets/icons/tabs/home.svg";
-import ShopIcon from "../../assets/icons/tabs/shop.svg";
 import CartIcon from "../../assets/icons/tabs/cart.svg";
-import WishlistIcon from "../../assets/icons/tabs/wishlist.svg";
+import HomeIcon from "../../assets/icons/tabs/home.svg";
 import ProfileIcon from "../../assets/icons/tabs/profile.svg";
+import ShopIcon from "../../assets/icons/tabs/shop.svg";
+import WishlistIcon from "../../assets/icons/tabs/wishlist.svg";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -17,42 +17,43 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => <HomeIcon width={20} height={20} />,
         }}
       />
       <Tabs.Screen
         name="shop"
         options={{
-          title: 'Shop',
+          title: "Shop",
           tabBarIcon: ({ color }) => <ShopIcon width={20} height={20} />,
         }}
       />
       <Tabs.Screen
         name="cart"
         options={{
-          title: 'Cart',
+          title: "Cart",
           tabBarIcon: ({ color }) => <CartIcon width={20} height={20} />,
         }}
       />
       <Tabs.Screen
         name="wishlist"
         options={{
-          title: 'Wishlist',
+          title: "Wishlist",
           tabBarIcon: ({ color }) => <WishlistIcon width={20} height={20} />,
         }}
       />
       <Tabs.Screen
-        name="profile "
+        name="profile"
         options={{
-          title: 'Profile',
+          title: "Profile",
           tabBarIcon: ({ color }) => <ProfileIcon width={20} height={20} />,
         }}
       />
